d049d292c0dde78cf441917cd0014d2cb1d4d2c8
Sai Madhuri <sai.k@Sais-Air.lan>
2024-01-16 21:19:11 -0500

    #1

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,21 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import { Request, Response } from "express";
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
+  const numAsNumber: number = parseInt(num, 10);
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
-
-  if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+  const fibN: number = fibonacci(parseInt(num));
+  let result: string;
+  result = '';
+  if (fibN < 0 || isNaN(fibN)) {
+    result = 'fibonacci(${numAsNumber}) is undefined';
   }
 
+  else if(fibN>=0 && !isNaN(fibN)){
+    result = 'fibonacci(${numAsNumber}) is ${fibN}';
+  }
   res.send(result);
 };
